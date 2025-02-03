8b44b5b4aafcb4405b8355e7727cd103002b1a51
alexisc7 <alexislz.k9@gmail.com>
2025-02-03 08:54:25 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///contacts.db"
+db = SQLAlchemy(app)
+
+
+# Crear modelo de base de datos
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    phone = db.Column(db.String(11), nullable=False)
+
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'phone': self.phone 
+        }
+    
+
+# Crear las tablas en la base de datos 
+with app.app_context():
+    db.create_all()
+
+
+
